8735af826f6ca2ce6b596c59979101f6cf28c569
Brian Moschel <Brian.Moschel@contractor.cengage.com>
2011-09-15 02:38:49 -0400

    latest

---
+++ public/steal/browser/phantomjs/client.js
@@ -0,0 +1,49 @@
+steal('jquery', function(){
+	steal.client = {}
+	steal.client.dataQueue = []
+	var id=0;
+	steal.client.trigger = function(type, data){
+//		console.log('TYPE: '+type+", data: "+data)
+		steal.client.dataQueue.push({
+			// workaround
+			id: ++id,
+			type: type,
+			data: data
+		})
+		if(type == "done"){
+			steal.client.phantomexit = true;
+		}
+	}
+	steal.client.sendData = function(){
+		var q = steal.client.dataQueue;
+		steal.client.dataQueue = [];
+		var params = encodeURIComponent(JSON.stringify(q));
+//		console.log('SENDING: '+params)
+		$.ajax({
+			url: "http://localhost:5555?" + params,
+			cache: false,
+			dataType: 'jsonp',
+			jsonp: false,
+			jsonpCallback: 'cb',
+			success: function(resp){
+				setTimeout(steal.client.sendData, 400);
+				if(resp){
+					var res = resp.fn();
+					steal.client.trigger('evaluated', res);
+				}
+				if (steal.client.phantomexit) {
+					// kills phantom process
+					setTimeout(function(){
+						alert('phantomexit')
+					}, 100)
+				}
+			}
+		})
+	}
+	steal.client.evaluate = function(script){
+		eval("var fn = "+script);
+		var res = fn();
+		return res;
+	};
+	setTimeout(steal.client.sendData, 1000);
+}, 'steal/browser/client.js')
---
+++ public/steal/browser/phantomjs/phantomjs.js
@@ -0,0 +1,114 @@
+steal('steal/browser', 'steal/browser/utils/rhinoServer.js', function(){
+	var page;
+	steal.browser.phantomjs = function(options){
+		steal.browser.call(this, options, 'phantomjs')
+	}
+	steal.extend(steal.browser.phantomjs, {
+		defaults:  {
+			
+		}
+	});
+	steal.browser.phantomjs.prototype = new steal.browser();
+	steal.extend(steal.browser.phantomjs.prototype, {
+		_startServer: function(){
+			this.server = new steal.browser.server(function(){
+				self._processData.apply(self, arguments);
+			})
+			// used as a cache to make sure we only run events once
+			this._evts = {}
+			var self = this;
+			self.evaluateResult = null;
+			this.bind("evaluated", function(data){
+				self.evaluateResult = data;
+				self.attr("evaluateInProgress", false);
+			})
+		},
+		open: function(page){
+			page = this._getPageUrl(page);
+			var verbose = this.options.print;
+			this.launcher = spawn(function(){
+				var cmd = "phantomjs steal/browser/phantomjs/launcher.js "+page+(verbose?  " -verbose": "");
+				if (java.lang.System.getProperty("os.name").indexOf("Windows") != -1) {
+					runCommand("cmd", "/C", cmd)
+				}
+				else {
+					var command = cmd + " > selenium.log 2> selenium.log &";
+					runCommand("sh", "-c", cmd);
+				}
+			})
+			// block until we're done
+			this.browserOpen = true;
+			while(this.browserOpen) {
+				java.lang.Thread.currentThread().sleep(1000);
+			}
+			return this;
+		},
+		// kill phantom and kill simple server
+		close: function(){
+			this.kill();
+			this.server.close();
+			this.browserOpen = false;
+		},
+		kill: function(){
+			spawn(function(){
+				if (java.lang.System.getProperty("os.name").indexOf("Windows") != -1) {
+					runCommand("cmd", "/C", 'taskkill /f /fi "Imagename eq phantomjs.exe" > NUL')
+				} else { // mac
+					runCommand("sh", "-c", "ps aux | awk '/phantomjs\\/launcher/ {print$2}' | xargs kill -9 &> /dev/null")
+				}
+			})
+		},
+		_processData: function(data){
+			var d = decodeURIComponent(unescape(data));
+//			print("_processData: "+d)
+			eval("var res = "+d)
+			// parse data into res
+			for (var i = 0; i < res.length; i++) {
+				evt = res[i];
+				// server receives duplicate requests for an unknown reason
+				// to work around this we check event ids to make sure we're not seeing a duplicate
+				if(this._evts[evt.id]) continue;
+				this._evts[evt.id] = true
+				var self = this;
+				(function(e){
+					spawn(function(){
+						self.trigger(e.type, e.data);
+					})
+				})(evt)
+			}
+		},
+		attr: function(name, value){
+			var self = this;
+			this.attrSynched = sync(function(){
+				if(typeof value === "undefined")
+					return self[name];
+				else self[name] = value;
+			});
+			return this.attrSynched();
+		},
+		sendJS: function(script){
+			// wait until previous finishes
+			while(this.attr("evaluateInProgress")) {
+				java.lang.Thread.currentThread().sleep(300);
+			}
+			this.server.sendJS(script);
+		},
+		evaluate: function(fn){
+			var evalText = fn.toString().replace(/\n|\r\n/g,""),
+				scriptText = "return steal.client.evaluate('"+evalText+"');";
+				
+			this.sendJS(scriptText);
+			this.attr("evaluateInProgress", true);
+			// wait until the "evaluated" event has been triggered, return the result
+			while(this.attr("evaluateInProgress")) {
+				java.lang.Thread.currentThread().sleep(300);
+			}
+			return this.evaluateResult;
+		},
+		injectJS: function(file){
+			var scriptText = readFile(file).replace(/\n|\r\n/g,"");
+			this.sendJS(scriptText);
+			this.attr("evaluateInProgress", true);
+		}
+	})
+})
