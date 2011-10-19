336c6e718f42dcb817feec17f47f82f63aecc471
Brian Moschel <Brian.Moschel@contractor.cengage.com>
2011-10-18 20:09:14 -0400

    Updating from source.

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,5 +1,6 @@
 Refraction.configure do |req|
   case req.query
-    when %r{^_escaped_fragment_=(.*)$} ; req.rewrite! :path => "/html/#{$1}.html", :query => ""
+    
+    when %r{^_escaped_fragment_=(.*)$} ; req.respond!(200, {'Content-Type' => 'text/html'}, File.read("/html/#{$1}.html"))
   end
 end
