8b9d4dad2662284dfaf8a0b7c75063e7f0e47009
David Luecke <daff@neyeon.com>
2012-11-29 09:18:52 -0700

    Updating from source.

--- public/can/dist/1.1.1/can.jquery-all.js
+++ public/can/dist/1.1.1/can.jquery-all.js
@@ -4517,7 +4517,6 @@
 	};
 	// ## can/observe/delegate/delegate.js
 
-
 	// ** - 'this' will be the deepest item changed
 	// * - 'this' will be any changes within *, but * will be the 
 	//     this returned
--- public/can/dist/1.1.1/can.observe.delegate.js
+++ public/can/dist/1.1.1/can.observe.delegate.js
@@ -7,7 +7,6 @@
 (function (can, window, undefined) {
 	// ## can/observe/delegate/delegate.js
 
-
 	// ** - 'this' will be the deepest item changed
 	// * - 'this' will be any changes within *, but * will be the 
 	//     this returned
