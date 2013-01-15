4f96cde0290fda4f296691a4c781c08a9eabf982
Alexis Abril <alexis@bitovi.com>
2013-01-15 00:32:49 -0600

    Manual build of public site. Added new beta dist.

---
+++ public/docs.js
@@ -0,0 +1,33 @@
+steal('can',
+	'funcunit',
+	'documentjs',
+	'jquery/build/lib.js')
+
+steal('steal/generate',
+	'steal/get',
+	'steal/build',
+	'steal/build/pluginify',
+	'steal/coffee',
+	'steal/less',
+	'steal/clean',
+	'steal/parse',
+	'steal/html/crawl')
+
+steal('can/construct',
+	'can/construct/proxy',
+	'can/control',
+	'can/control/route',
+	'can/control/plugin',
+	'can/model',
+	'can/observe',
+	'can/observe/attributes',
+	'can/observe/backup',
+	'can/observe/delegate',
+	'can/observe/validations',
+	'can/route',
+	'can/view/ejs',
+	'can/util/fixture',
+	'can/view/modifiers',
+	'can/view/mustache')
+
+// .then('canui')
